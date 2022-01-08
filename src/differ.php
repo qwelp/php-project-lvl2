f155b13fb7161de8f9aa317743120bd5b7e52cfd
Vladimir Bun <qwelp@mail.ru>
2022-01-08 14:28:45 +0300

    Add tests

--- src/differ.php
+++ src/differ.php
@@ -10,16 +10,41 @@ function genDiff(string $firstFile, string $secondFile): string
 
     $firstFile = file_get_contents($firstFile);
     $secondFile = file_get_contents($secondFile);
-
-    if (!$firstFile || !$secondFile) {
-        return '';
-    }
-
     $objFirstFile = decodeJsonFileDiff($firstFile);
     $objSecondFile = decodeJsonFileDiff($secondFile);
 
+    return renderDiff($objFirstFile, $objSecondFile);
+}
 
-    return "123";
+function renderDiff(array $firstFile, array $secondFile): string
+{
+    $result = [];
+    $result[] = "{";
+
+    foreach ($firstFile as $k => $v) {
+        if (isset($secondFile[$k])) {
+            if ($v === $secondFile[$k]) {
+                $result[] = "    " . $k . ": " . $secondFile[$k];
+            } else {
+                $result[] = "  - " . $k . ": " . $v;
+                $result[] = "  + " . $k . ": " . $secondFile[$k];
+            }
+        } else {
+            $result[] = "  - " . $k . ": " . $v;
+        }
+    }
+
+    foreach ($secondFile as $k => $v) {
+        $objFirstFileKeys = array_keys($firstFile);
+
+        if (!in_array($k, $objFirstFileKeys)) {
+            $result[] = "  + " . $k . ": " . $v;
+        }
+    }
+
+    $result[] = "}";
+
+    return implode(PHP_EOL, $result) . PHP_EOL;
 }
 
 function decodeJsonFileDiff(string $file): array
--- tests/DefferTest.php
+++ tests/DefferTest.php
@@ -8,13 +8,31 @@ use function Differ\Differ\genDiff;
 
 class DefferTest extends TestCase
 {
+    const PATH_FIXTURE = __DIR__ . "/fixtures/";
+    const FILE_1 = self::PATH_FIXTURE . "file1.json";
+    const FILE_2 = self::PATH_FIXTURE . "file2.json";
+    const FILE_EMPTY = self::PATH_FIXTURE . "fileEmpty.json";
+
     public function testDeffer(): void
     {
-        $path = __DIR__ . "/fixtures/";
-        $file1 = $path . "file1.json";
-        $file2 = $path . "file2.json";
-        $diff = genDiff($file1, $file2);
+        $diff = genDiff(self::FILE_1, self::FILE_2);
 
-        $this->assertEquals('123', $diff);
+        $expected = "{" . PHP_EOL;
+        $expected .= "    host: hexlet.io" . PHP_EOL;
+        $expected .= "  - timeout: 50" . PHP_EOL;
+        $expected .= "  + timeout: 20" . PHP_EOL;
+        $expected .= "  - proxy: 123.234.53.22" . PHP_EOL;
+        $expected .= "  - follow: " . PHP_EOL;
+        $expected .= "  + verbose: 1" . PHP_EOL;
+        $expected .= "}". PHP_EOL;
+
+        $this->assertEquals($expected, $diff);
+    }
+
+    public function testException()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('File not found.');
+        genDiff('', self::FILE_2);
     }
 }
