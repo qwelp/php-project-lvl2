4ee741d6ed054c5baf7f2099f4104de4298d57f8
Vladimir Bun <qwelp@mail.ru>
2022-01-08 11:19:28 +0300

    Fix

--- src/differ.php
+++ src/differ.php
@@ -4,6 +4,10 @@ namespace Differ\Differ;
 
 function genDiff(string $firstFile, string $secondFile): string
 {
+    if (!file_exists($firstFile) || !file_exists($secondFile)) {
+        throw new \Exception("File not found.");
+    }
+
     $firstFile = file_get_contents($firstFile);
     $secondFile = file_get_contents($secondFile);
 
