983dda942ba9d98a8400079eaa0e041f5ba64b88
Vladimir Bun <qwelp@mail.ru>
2022-03-08 12:39:59 +0300

    Fix

--- src/Differ.php
+++ src/Differ.php
@@ -17,33 +17,32 @@ function genDiff(string $pathToFile1, string $pathToFile2): string
         throw new \Exception("File empty.");
     }
 
-    $objFile1 = json_decode($file1, true);
-    $objFile2 = json_decode($file2, true);
-    return render($objFile1, $objFile2);
+    $firstFile = json_decode($file1, true);
+    $secondFile = json_decode($file2, true);
+    return render($firstFile, $secondFile);
 }
 
-function render(array $objFile1, array $objFile2): string
+function render(array $firstFile, array $secondFile): string
 {
     $result =  [];
-    $data = array_merge($objFile1, $objFile2);
+    $data = array_merge($firstFile, $secondFile);
     foreach ($data as $key => $value) {
-        if (array_key_exists($key, $objFile1) && array_key_exists($key, $objFile2)) {
-            if ($objFile1[$key] === $objFile2[$key]) {
-                $result[$key] = "    {$key}: " . stringToBool($objFile1[$key]);
+        if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile)) {
+            if ($firstFile[$key] === $secondFile[$key]) {
+                $result[$key] = "    {$key}: " . stringToBool($firstFile[$key]);
             } else {
-                $result[$key] = "  - {$key}: " . stringToBool($objFile1[$key]);
-                $result[$key . 0] = "  + {$key}: " . stringToBool($objFile2[$key]);
+                $result[$key] = "  - {$key}: " . stringToBool($firstFile[$key]);
+                $result[$key . 0] = "  + {$key}: " . stringToBool($secondFile[$key]);
             }
             continue;
         }
 
-        if (array_key_exists($key, $objFile1)) {
-            $result[$key] = "  - {$key}: " . stringToBool($objFile1[$key]);
+        if (array_key_exists($key, $firstFile)) {
+            $result[$key] = "  - {$key}: " . stringToBool($firstFile[$key]);
             continue;
         }
-        $result[$key] = "  + {$key}: " . stringToBool($objFile2[$key]);
+        $result[$key] = "  + {$key}: " . stringToBool($secondFile[$key]);
     }
-
     ksort($result);
     return "{" . PHP_EOL . implode(PHP_EOL, $result) . PHP_EOL . "}" . PHP_EOL;
 }
